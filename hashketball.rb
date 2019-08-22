e06f2b4a8396a270c022b2054841e44d2d7188c4
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 10:05:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -214,6 +214,19 @@ def most_points_scored
   return highestplayer
 end 
 
+def winning_team
+
+   top_team = {}
+  game_hash.values.each do |team_info|
+    team_points = 0
+    team_info[:players].each do |player|
+      team_points += player[:points]
+    end
+    top_team[team_info[:team_name]] = team_points
+  end
+  top_team.key(top_team.values.max) 
+end
+
   def player_with_longest_name 
     longest_name = []
     game_hash.each{|team, details|
